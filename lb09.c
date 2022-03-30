3bad7c3a11931923ffdeca9a082bd02a07c14c28
Patho <62456068+Path0@users.noreply.github.com>
2022-03-30 17:57:25 +0000

    1

--- lb09.c
+++ lb09.c
@@ -1,18 +1,30 @@
 #include <stdio.h>
 #include <math.h>
 
+int isPerfectSquare(int);
+
 int main()
 {
-
+  isPerfectSquare(64);
   return 0;
 }
 
 int getInput()
 {
-  int input; //User entered  
+  int input; //User entered "b" value
   
-  printf("______");
+  printf("Enter starting value -> ");
   scanf("%d", &input);
 
   return input;
+}
+
+int isPerfectSquare(int number)
+{
+  double square;
+
+  square = sqrt(number);
+
+  printf("%d", (int)(square * 100) % 100);
+  return ((int)(square * 100) % 100);
 }
