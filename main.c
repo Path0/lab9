4325a7c275cbb6356b5b528bf0f9b5a51740e007
pathopickax <>
2022-03-30 17:42:09 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,41 @@
+/*****+--***---*-*-*---*-**-*---*---**-*****--******************************
+*
+*  Lab #: 9
+*
+*  Academic Integrity Statement:
+*
+*  We have not used source code obtained from any other unauthorized source,
+*  either modified or unmodified.  Neither have we provided access to our code
+*  to another. The effort we are submitting is our own original work.
+*
+*  Program Description:
+*
+******+--***---*-*-*---*-**-*---*---**-*****--*****************************/
+
+/*****+-**------*---**-*-**---*-*-*--*-*****--******************************
+*
+*  Function Information
+*
+*  Name of Function:
+*
+*  Function Return Type:
+*
+*  Parameters (list data type, name, and comment one per line):
+*  1.
+*  2.
+*  3.
+*
+*  Function Description: 
+*
+******+-**------*---**-*-**---*-*-*--*-*****--*****************************/
+#include <stdio.h>
+#include <math.h>
+
+//Declaring Fumctions
+
+
+int main(void) 
+{
+  printf("Hello World\n");
+  return 0;
+}
